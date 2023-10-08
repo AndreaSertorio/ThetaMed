24f55529cedb6d60966fd444fad83a86844d7a9f
AndreaSertorio <andrea.sertorio22@gmail.com>
2023-10-08 13:31:56 +0200

    footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => (
   <footer className="footer p-4  bg-hippie-green mt-8 border-t border-silver-chalice">
-    <div>© 2023 ThetaMed</div>
+    <div>© 2023 EcoTreDi</div>
   </footer>
 );
 
