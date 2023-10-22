4fc9829993ef7b3759b5f5331061e4c284f2b01d
AndreaSertorio <andrea.sertorio22@gmail.com>
2023-10-22 23:32:36 +0200

    aggiornato imprecisioni chi siamo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Image from 'next/image'; // Import the Image component from Next.js
 import '../globals.css';
 
 const Page: React.FC = () => (
@@ -15,21 +16,23 @@ const Page: React.FC = () => (
 
         {/* Prima sezione con immagine e testo a fianco */}
         <div className="flex flex-col md:flex-row space-y-6 md:space-y-10 md:space-x-2 w-full">
-          <div className="flex flex-col justify-center space-y-4  space-x-10 w-full md:w-1/2 md:order-last">
+          <div className="flex flex-col justify-center space-y-4 space-x-10 w-full md:w-1/2 md:order-last">
             <h2 className="text-2xl font-bold mb-2">Chi Siamo</h2>
-            <p className="text-lg mb-4">ECOTREDI Multisonda è un'impresa leader nel campo biomedico, con un focus sull'innovazione delle tecniche ecografiche e l'intelligenza artificiale (AI). La nostra missione è rivoluzionare le metodiche di imaging attraverso l'ecografia, offrendo soluzioni più precise, efficienti e personalizzate per la diagnosi.</p>
+            <p className="text-lg mb-4">ECOTREDI Multisonda è un&apos;impresa leader nel campo biomedico, con un focus sull&apos;innovazione delle tecniche ecografiche e l&apos;intelligenza artificiale (AI). La nostra missione è rivoluzionare le metodiche di imaging attraverso l&apos;ecografia, offrendo soluzioni più precise, efficienti e personalizzate per la diagnosi.</p>
             <h3 className="text-xl font-bold mb-2">La nostra forza e know-how:</h3>
             <ul className="list-disc pl-5 space-y-4 text-lg">
-              <li>Ricerca e sviluppo nell'ambito biomedico e dell’intelligenza artificiale.</li>
-              <li>Formazione specializzata nel campo della radiologia per immagini legata all'AI.</li>
+              <li>Ricerca e sviluppo nell&apos;ambito biomedico e dell’intelligenza artificiale.</li>
+              <li>Formazione specializzata nel campo della radiologia per immagini legata all&apos;AI.</li>
               <li>Collaborazione stretta con le Unità Operative della Fondazione Policlinico Universitario Campus Bio-Medico.</li>
             </ul>
           </div>
-          <img src="/image8.png" alt="Imaging avanzato" className="w-full md:w-1/2 md:order-first mb-4" />
+          <Image src="/image8.png" alt="Imaging avanzato" width={500} height={500} className="md:w-1/2 md:order-first mb-4" /> 
+          {/* Please adjust width and height attributes according to your needs */}
         </div>
 
         {/* Seconda immagine che occupa tutta la larghezza */}
-        <img src="/image9.png" alt="Descrizione Immagine 2" className="w-full" />
+        <Image src="/image9.png" alt="Descrizione Immagine 2" width={1000} height={500} className="w-full" />
+        {/* Please adjust width and height attributes according to your needs */}
 
       </div>
     </main>
